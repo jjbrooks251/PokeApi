b24f6dadba212e832c067622dba5fffe85c5aa25
jjbrooks251 <jjbrooks251@outlook.com>
2019-08-09 10:46:12 +0100

    serach by name/num added + tested

--- PokeRequests/src/main/java/com/qa/rest/RequestController.java
+++ PokeRequests/src/main/java/com/qa/rest/RequestController.java
@@ -33,4 +33,17 @@ public class RequestController {
 		return restTemplate.exchange("https://pokeapi.co/api/v2/pokemon/"+name, HttpMethod.GET, entity, Object.class);
 	}
 	
+	@GetMapping(value = "/getNum/{number}")
+	public ResponseEntity<Object> getPokeByNum(@PathVariable("number") int num){
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("user-agent",
+				"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36");
+		HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
+
+		
+		return restTemplate.exchange("https://pokeapi.co/api/v2/pokemon/"+num, HttpMethod.GET, entity, Object.class);
+		
+	}
+	
 }
